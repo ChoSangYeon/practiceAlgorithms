316bc8e2cdcdd65fe1245ffbc2b8ba54c5a83f9f
ChoSangYeon <113081584+ChoSangYeon@users.noreply.github.com>
2023-09-22 22:03:45 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,5 +1,5 @@
 def solution(numbers):
     new_num = []
     for i in range(len(numbers)):
-        new_num.append(int(numbers[i]) * 2)
+        new_num.append((numbers[i]) * 2)
     return new_num
