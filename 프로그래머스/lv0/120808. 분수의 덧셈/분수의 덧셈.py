2709fe8d90f4ec92e19f8d7b6aeb7206fe94a12b
ChoSangYeon <113081584+ChoSangYeon@users.noreply.github.com>
2023-09-24 01:42:25 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,10 +1,19 @@
-from math import gcd
+def find_gcd(a,b):
+    while b:
+        a,b = b, a%b
+    return a
+
+def simplify_fraction(numer, denom):
+    common_divisor = find_gcd(numer, denom)
+    simplified_numer = numer // common_divisor
+    simplified_denom = denom // common_divisor
+    return simplified_numer, simplified_denom
 
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    numer3 = numer1*denom2+numer2*denom1
-    denom3 = denom1*denom2
-    check_gcd = gcd(numer3, denom3)
-    answer.append(numer3/check_gcd)
-    answer.append(denom3/check_gcd)    
-    return answer
+    common_denominator = denom1 * denom2
+    new_numer1 = numer1 * denom2
+    new_numer2 = numer2 * denom1 
+    result_numer = new_numer1 + new_numer2
+
+    simplified_result = simplify_fraction(result_numer, common_denominator)
+    return simplified_result
