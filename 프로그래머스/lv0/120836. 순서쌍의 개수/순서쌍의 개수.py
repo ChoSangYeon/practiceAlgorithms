26352c5d0a62315c0102bfb2ffbfbc59372c06b2
ChoSangYeon <113081584+ChoSangYeon@users.noreply.github.com>
2023-10-01 16:35:14 +0900

    [level 0] Title: 순서쌍의 개수, Time: 79.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,6 @@
 def solution(n):
-    lst = []
+    answer = 0
     for i in range(1, n+1):
         if n % i == 0:
-            lst.append(i)
-    return len(lst)
+            answer += 1
+    return answer
